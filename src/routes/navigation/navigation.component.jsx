df0ecf8985ecd79bfc812c35492719f136cbfeb0
Minsu Lee <usnimeel@gmail.com>
2022-06-12 23:21:07 -0300

    Add: Firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,6 +15,7 @@ const Navigation = () => {
                 </div>
                 <div className="nav-links-container">
                     <Link className="nav-link" to='shop'>SHOP</Link>
+                    <Link className="nav-link" to='sign-in'>SIGN IN</Link>
                 </div>
             </div>
             <Outlet />
